48311fa982c04a756c6d151966fa8efa92ceb385
wsuyep <willy.suyep@move.com>
2019-08-27 01:18:15 -0700

    bugfix - prevent server crashing due to duplicate shopName

--- request-handlers/register-owner.js
+++ request-handlers/register-owner.js
@@ -15,19 +15,30 @@ const hat = require('hat');
 const registerOwner = (body, dbHelper, cb) => {
     console.log('registerOwnder request received: ' + JSON.stringify(body));
     const { shopName, address, phone } = body;
-    if(!(shopName && address && phone)){
+    if (!(shopName && address && phone)) {
         cb('Missing parameters');
         return;
     }
     const shopId = uuid();
     const apiToken = hat();
-    const query = `INSERT INTO shops VALUES("${shopId}","${shopName}","${apiToken}","${phone}","${address}")`;
-    dbHelper.updateTable(query, (err) => {
+    const shopLookupSql = `SELECT * FROM shops WHERE shop_name="${shopName}"`;
+    dbHelper.getRecords(shopLookupSql, (err, rows) => {
         if (err) {
             cb(err, null);
             return;
         }
-        cb(null, { shopId, apiToken });
+        if (rows && rows.length === 1) {
+            cb('shop name is already registered', null);
+            return;
+        }
+        const query = `INSERT INTO shops VALUES("${shopId}","${shopName}","${apiToken}","${phone}","${address}")`;
+        dbHelper.updateTable(query, (err) => {
+            if (err) {
+                cb(err, null);
+                return;
+            }
+            cb(null, { shopId, apiToken });
+        });
     });
 };
 
